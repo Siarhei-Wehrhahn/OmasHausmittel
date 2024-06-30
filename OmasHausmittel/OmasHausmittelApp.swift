0614c43d9d24392d89bb81b5fee12c48817c9d18
Sergej <sergei1997.sw@gmail.com>
2024-06-30 17:42:54 +0200

    Initial Commit

---
+++ OmasHausmittel/OmasHausmittelApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmasHausmittelApp.swift
+//  OmasHausmittel
+//
+//  Created by Siarhei Wehrhahn on 30.06.24.
+//
+
+import SwiftUI
+
+@main
+struct OmasHausmittelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
